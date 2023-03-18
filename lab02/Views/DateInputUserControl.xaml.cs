53a79a27c1b242a23ce6c7790e51906e50038737
VictorSoviak <inode16@gmail.com>
2023-03-18 04:30:34 +0200

    Add project files.

---
+++ lab02/Views/DateInputUserControl.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace lab02
+{
+    public partial class DateInputUserControl : UserControl
+    {
+        private DateInfo dateFromUser;
+
+        public DateInputUserControl()
+        {
+            InitializeComponent();
+            dateFromUser = new DateInfo();
+            DataContext = dateFromUser;
+        }
+
+
+    }
+}
